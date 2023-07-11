5c5b0b7f04b2502fb15b721305bf53c25b4e04b1
zokirjanov <zokirjonovshohruxbek1016@gmail.com>
2023-07-11 20:58:42 +0500

    Font Awesome Added

--- Maui.DesignApp/MauiProgram.cs
+++ Maui.DesignApp/MauiProgram.cs
@@ -11,6 +11,10 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+
+				fonts.AddFont("Brands-Regular-400.ttf", "FAB");
+				fonts.AddFont("Free-Regular-400.ttf", "FAR"); 
+				fonts.AddFont("Free-Solid-900.ttf", "FAS");
 			});
 
 		return builder.Build();
